be9b8660c3d8efa475c09829189c49df7abc7905
bosshog23 <bosshog23@users.noreply.github.com>
2017-01-10 15:31:44 -0800

    Update League Settings.R

--- R Scripts/Functions/League Settings.R
+++ R Scripts/Functions/League Settings.R
@@ -8,16 +8,16 @@
 ###########################
 
 #Season
-season <- 2014
-weekNo <- 0   # Set weekNo = 0 for seasonal projections
+season <- 2016
+weekNo <- 13   # Set weekNo = 0 for seasonal projections
 
 #Roster
 numQBstarters <- 1
 numRBstarters <- 2
-numWRstarters <- 2
+numWRstarters <- 3
 numTEstarters <- 1
-numTotalStarters <- 7
-numTotalPlayers <- 20
+numTotalStarters <- 9
+numTotalPlayers <- 25
 
 #League settings
 defaultCap <- 200           #what the typical cap is for your service (ESPN, Yahoo, etc.) -- used for placing "avg cost" in context
@@ -40,36 +40,36 @@ finalVarNames <- c("name","pos","team","sourceName","player","playerID","season"
 
 #Scoring
 passAttMultiplier <- 0      #0 pts per passing attempt
-passCompMultiplier <- 0     #0 pts per passing completion
-passIncompMultiplier <- 0   #0 pts per passing incompletion
+passCompMultiplier <- .1    #0 pts per passing completion
+passIncompMultiplier <- -.1   #0 pts per passing incompletion
 passYdsMultiplier <- (1/25) #1 pt per 25 passing yds
 passTdsMultiplier <- 4      #4 pts per passing td
-passIntMultiplier <- -3     #-3 pts per passing interception
+passIntMultiplier <- -1     #-3 pts per passing interception
 rushAttMultiplier <- 0      #0 pts per rushing attempt
 rushYdsMultiplier <- (1/10) #1 pt per 10 rushing yds
 rushTdsMultiplier <- 6      #6 pts per rushing touchdown
-recMultiplier <- 0          #0 pts per reception
-recYdsMultiplier <- (1/8)   #1 pt per 8 receiving yds
+recMultiplier <- .5          #0 pts per reception
+recYdsMultiplier <- (1/10)   #1 pt per 10 receiving yds
 recTdsMultiplier <- 6       #6 pts per receiving touchdown
 returnTdsMultiplier <- 6    #6 pts per return touchdown
 twoPtsMultiplier <- 2       #2 pts per 2-point conversion
-fumlMultiplier <- -3        #-3 pts per fumble lost
+fumlMultiplier <- -2        #-3 pts per fumble lost
 
 scoringRules <- list(
     QB = data.frame(dataCol = c("passYds", "passTds", "passInt", "rushYds", "rushTds", "twoPts", "fumbles"),
-                    multiplier = c(1/25, 4, -3, 1/10, 6, 2, -3 )),
+                    multiplier = c(1/25, 4, -1, 1/10, 6, 2, -2 )),
     RB = data.frame(dataCol = c("rushYds", "rushTds", "rec", "recYds", "recTds", "returnTds", "twoPts", "fumbles"),
-                    multiplier = c(1/10, 6, 0, 1/8, 6, 6, 2, -3)), 
+                    multiplier = c(1/10, 6, .5, 1/10, 6, 6, 2, -2)), 
     WR = data.frame(dataCol = c("rushYds", "rushTds", "rec", "recYds", "recTds", "returnTds", "twoPts", "fumbles"),
-                    multiplier = c(1/10, 6, 0, 1/8, 6, 6, 2, -3)),
+                    multiplier = c(1/10, 6, .5, 1/10, 6, 6, 2, -2)),
     TE = data.frame(dataCol = c("rushYds", "rushTds", "rec", "recYds", "recTds", "returnTds", "twoPts", "fumbles"),
-                    multiplier = c(1/10, 6, 0, 1/8, 6, 6, 2, -3)),
+                    multiplier = c(1/10, 6, .5, 1/10, 6, 6, 2, -2)),
     K = data.frame(dataCol = c("xp", "fg", "fg0019", "fg2029", "fg3039", "fg4049", "fg50"),
                    multiplier = c(1, 3, 3, 3, 3, 4, 5)),
     DST = data.frame(dataCol = c("dstFumlRec", "dstInt", "dstSafety", "dstSack", "dstTd", "dstBlk"),
-                     multiplier = c(2, 2, 2, 1, 6, 1.5)),
+                     multiplier = c(2, 2, 6, 1, 6, 4)),
     ptsBracket = data.frame(threshold = c(0, 6, 20, 34, 99),
-                             points = c(10, 7, 4, 0, -4))
+                             points = c(12, 10, 4, -2, -5))
   )
 
 
